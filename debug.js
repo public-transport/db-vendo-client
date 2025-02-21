a79364223050ce6181ed8696bb48336ed7871f37
McToel <theo.doellmann@gmx.de>
2025-02-21 20:10:48 +0100

    removed temp debug file

--- debug.js
+++
@@ -1,7 +0,0 @@
-import {createClient} from './index.js';
-import {profile} from './p/dbnav/index.js';
-
-const client = createClient(profile, 'hafas-client-debug');
-
-const journeys = await client.journeys('8000105', '8000261', {results: 1});
-console.log(journeys);
